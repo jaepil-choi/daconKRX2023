28bd3353113523eb7a090456bf19a8470da7ea96
jaepil-choi <chljeffreyz@gmail.com>
2023-07-23 19:52:50 +0900

    [FEAT] : Apply Inho's ML (xgb, lgbm) to my datasets - debug needed

--- submission_config.py
+++ submission_config.py
@@ -12,10 +12,20 @@ adjclose_df_PATH = DATA_PATH / 'adjClose_20140101_20230705.pickle'
 adjhigh_df_PATH = DATA_PATH / 'adjHigh_20140101_20230705.pickle'
 adjlow_df_PATH = DATA_PATH / 'adjLow_20140101_20230705.pickle'
 adjopen_df_PATH = DATA_PATH / 'adjOpen_20140101_20230705.pickle'
-volume_df_PATH = DATA_PATH / 'volume_20140101_20230705.pickle'
+volume_df_PATH = DATA_PATH / 'volume_df_20140101_20230705.pickle'
 dollarvolume_df_PATH = DATA_PATH / 'dollarvolume_df_20140101_20230705.pickle'
 marketcap_df_PATH = DATA_PATH / 'marketcap_df_20140101_20230705.pickle'
 
 ## Param configs
 
-PORTFOLIO_DATE = '2023-05-30' # SimOS date
+# train (custom)
+TRAIN_START = '2021-06-01'
+
+# SimOS
+PORTFOLIO_DATE = '2023-05-30' 
+SIMOS_START = '2023-05-31'
+SIMOS_END = '2023-06-21'
+
+WINDOWS = {
+    'rdvadv': 20,
+}
